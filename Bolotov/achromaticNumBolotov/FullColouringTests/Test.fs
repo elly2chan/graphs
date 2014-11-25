98a47f96ff733b6af357853d73ade1501dd6737d
Bolotov Sergey <bolotov.s.s@yandex.ru>
2014-11-25 21:32:21 +0300

    Added test project

---
+++ Bolotov/achromaticNumBolotov/FullColouringTests/Test.fs
@@ -0,0 +1,13 @@
+﻿namespace AchromaticNum.FullColouringTests
+
+open NUnit.Framework
+open FsUnit
+
+let complete (count: int) = 
+    
+
+[<TestFixture>]
+type ``Main Tests``() = 
+    [<Test>]
+    member this.``Complete graphs`` () = 
+        Assert.GreaterOrEqual
